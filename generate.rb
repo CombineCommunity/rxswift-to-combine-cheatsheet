747dd95e9d6e48f3b39138889fb0623869fe2743
freak4pc <freak4pc@gmail.com>
2019-06-14 23:56:59 +0300

    purple -> magenta in generation script

--- generate.rb
+++ generate.rb
@@ -17,7 +17,7 @@ output = "# RxSwift to Combine Cheatsheet\n" +
          "This is a Cheatsheet for [RxSwift](https://github.com/ReactiveX/RxSwift) developers interested in Apple's new [Combine](https://developer.apple.com/documentation/combine) framework.\n\n" + 
          "It's based on the following blog post: [https://medium.com/gett-engineering/rxswift-to-apples-combine-cheat-sheet-e9ce32b14c5b](https://medium.com/gett-engineering/rxswift-to-apples-combine-cheat-sheet-e9ce32b14c5b)\n\n"
      
-puts "Rebuilding README.md ...".purple
+puts "Rebuilding README.md ...".magenta
 
 docs.each { |file, title|
     ## Generate markdown for the specific section
@@ -86,7 +86,7 @@ docs.each { |file, title|
 output += "# Contributing\n"
 output += "Add any data/operators to the appropriate CSV files in the **Data** folder, run `bundle install` and `generate.rb`.\n\nFinally, commit the changes and submit a Pull Request."
 
-puts "Writing README.md ...".purple
+puts "Writing README.md ...".magenta
 File.write('README.md', output)
 
 puts "🎉 All done! 🎉".green
